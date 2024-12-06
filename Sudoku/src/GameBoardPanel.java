0e07ea32eb7f4a98d7fbb8d52f8b0ae7a2578fa9
tiffanycatherine <tiffanycatherine08@gmail.com>
2024-12-06 23:37:02 +0700

    Group members

--- Sudoku/src/GameBoardPanel.java
+++ Sudoku/src/GameBoardPanel.java
@@ -1,3 +1,14 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #7
+ * 1 - 5026231011 - William Bryan Pangestu
+ * 2 - 5026231022 - Tiffany Catherine Prasetya
+ * 3 - 5026231081 - Oryza Reynaleta Wibowo
+ */
+
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;

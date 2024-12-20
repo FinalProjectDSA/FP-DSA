a28f47e23951c2d18d8f94e447a5490e23118c68
tiffanycatherine <tiffanycatherine08@gmail.com>
2024-12-20 21:43:05 +0700

    Fix AI mode and restart button

--- TicTacToe/src/AIPlayer.java
+++ TicTacToe/src/AIPlayer.java
@@ -2,8 +2,7 @@
  * Abstract superclass for all AI players with different strategies.
  * To construct an AI player:
  * 1. Construct an instance (of its subclass) with the game Board
- * 2. Call setSeed() to set the computer's seed
- * 3. Call move() which returns the next move in an int[2] array of {row, col}.
+ * 2. Call move() which returns the next move in an int[2] array of {row, col}.
  *
  * The implementation subclasses need to override abstract method move().
  * They shall not modify Cell[][], i.e., no side effect expected.
@@ -20,12 +19,6 @@ public abstract class AIPlayer {
     /** Constructor with reference to game board */
     public AIPlayer(Board board) {
         cells = board.cells;
-    }
-
-    /** Set/change the seed used by computer and opponent */
-    public void setSeed(Seed seed) {
-        this.mySeed = seed;
-        oppSeed = (mySeed == Seed.CROSS) ? Seed.NOUGHT : Seed.CROSS;
     }
 
     /** Abstract method to get next move. Return int[2] of {row, col} */
